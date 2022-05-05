0f5c0b7c59b3069157b7a8fc455ff3b06ad6d9ff
Anjani <pranav.ch.12@outlook.com>
2022-05-05 21:54:36 +0200

    Added tailwind container config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    container: {
+      center: true
+    }
+  }
 }
