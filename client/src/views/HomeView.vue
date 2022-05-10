f7261a36ebc0fd80514c8fb82cc5772a44936efc
Anjani <pranav.ch.12@outlook.com>
2022-05-10 13:41:30 +0200

    Fixed home container issu

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,9 +2,9 @@
   <main class="main-wrapper">
     <section class="container flex flex-col-reverse lg:flex-row items-center gap-12 mt-14 lg:mt-28">
       <div class="flex flex-1 flex-col items-center lg:items-start">
-        <h2 class="text-bookmark-blue text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">Overwatch</h2>
+        <h2 class="text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">Overwatch</h2>
 
-        <p class="text-bookmark-grey text-lg text-center lg:text-left mb-3">
+        <p class="text-lg text-center lg:text-left mb-3">
           Overwatch is a colorful team-based action game starring a diverse cast of powerful heroes. Travel the world, build a team, and contest objectives in exhilarating 6v6 combat.
         </p>
         
@@ -13,7 +13,7 @@
         </div>
       </div>
 
-      <div class="flex justify-center flex-1 mb-10 md:mb-4 lg:mb-0">
+      <div class="flex justify-end flex-1 mb-10 md:mb-4 lg:mb-0">
         <img class="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-full md:h-full lg:w-4/6 lg:h-4/6" src="../assets/overwatch-logo.svg" alt="overwatch-logo" />
       </div>
     </section>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,16 +2,17 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     container: {
-      center: true
+      center: true,
+      padding: '1rem',
+      screens: {
+        'xs': '500px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px'
+      }
     },
-    screens: {
-      'xs': '500px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px'
-    }
   },
   plugins: [
     require('tw-elements/dist/plugin')
